f7659d2a8f1b1de15076639ea6a5bd1f33476898
RNCST <rk51320928@gmail.com>
2021-02-08 13:13:40 +0900

    HelloWorld.java 추가

---
+++ java210208/src/com/base/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.base;
+
+public class HelloWorld {
+	int age = 20;//전역변수
+	public static void main(String[] args) {
+		HelloWorld hw = new HelloWorld();
+		hw.age = 29;
+		System.out.println("당신의 나이는 "+hw.age+"입니다.");
+		
+
+	}
+
+}
